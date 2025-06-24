29cd07a5e50f7d3ed8d617b19898e15f93dc882d
Joshua Bulyan Zebua <123555895+TimesCoder@users.noreply.github.com>
2025-06-24 23:26:06 +0700

    Delete Assets/Scripts/Untuk warga.cs

--- Assets/Scripts/Untuk warga.cs
+++
@@ -1,8 +0,0 @@
-// void OnTriggerEnter(Collider other)
-// {
-//     if (other.CompareTag("Player"))
-//     {
-//         FindObjectOfType<CitizenRescueMission>().OnCitizenSaved();
-//         Destroy(gameObject); // hapus warga dari scene
-//     }
-// }
